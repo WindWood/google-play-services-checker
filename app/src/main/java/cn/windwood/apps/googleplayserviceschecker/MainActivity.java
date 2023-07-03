4249e16cdc925f1be56a574b01541acee17f5949
WindWood <windwood@gmail.com>
2023-07-03 17:16:20 +0800

    Setup UI + design layout + add view binding support + create code main frame

--- app/src/main/java/cn/windwood/apps/googleplayserviceschecker/MainActivity.java
+++ app/src/main/java/cn/windwood/apps/googleplayserviceschecker/MainActivity.java
@@ -1,14 +1,30 @@
 package cn.windwood.apps.googleplayserviceschecker;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import cn.windwood.apps.googleplayserviceschecker.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    public void servicesInfo(View view) {
+    }
+
+    public void storeInfo(View view) {
+    }
+
+    public void aboutInfo(View view) {
     }
 }
